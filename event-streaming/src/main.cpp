086a7ca839e073d17a0511d1454592c626fd5bc2
Tomas <tomas.ew88@gmail.com>
2025-08-06 19:13:16 +0300

    Rm comments

--- event-streaming/src/main.cpp
+++ event-streaming/src/main.cpp
@@ -3,26 +3,6 @@
 
 int main()
 {
-	//auto streamer = EventStreamer();
-	//std::map<std::string, PropertyType> properties = {
-	//	{ "key1", PropertyType::INT },
-	//	{ "key2", PropertyType::DECIMAL }
-	//};
-
-	//auto eventType = EventType("TestEvent", properties);
-
-	//streamer.RegisterEventType(eventType);
-	//streamer.Subscribe(eventType, "127.0.0.1");
-
-	//std::map<std::string, void*> values = {
-	//	{ "key1", (void*)new int(42) },
-	//	{ "key2", (void*)new double(42.2) },
-	//};
-	//auto testEvent = Event(
-	//	eventType,
-	//	values
-	//);
-	//streamer.ProduceEvent(testEvent);
 	std::cout << "Event streaming application started." << std::endl;
 
 	Application app;
