6994aad91a565fef972d724fd8c025706daa08d6
Tomas <tomas.ew88@gmail.com>
2025-08-09 12:02:14 +0300

    Separate declaration and definition

--- event-streaming/src/application/utils.h
+++ event-streaming/src/application/utils.h
@@ -1,14 +1,9 @@
+#pragma once
+
 #include <cstdint>
 #ifdef __linux__
     #include <arpa/inet.h>
 #endif
 
-uint32_t HostToBigEndian32(uint32_t number)
-{
-    return htonl(number);   
-}
-
-uint32_t BigEndianToHost32(uint32_t number)
-{
-    return ntohl(number);
-}
+uint32_t HostToBigEndian32(uint32_t number);
+uint32_t BigEndianToHost32(uint32_t number);
