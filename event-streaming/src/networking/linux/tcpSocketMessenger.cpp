0ae241ca99e299d166e8a80fc019cbccd2577865
Tomas <tomas.ew88@gmail.com>
2025-08-09 12:03:28 +0300

    TCP send message add message boundaries by setting first 4 bytes of a message to the server message buffer length

--- event-streaming/src/networking/linux/tcpSocketMessenger.cpp
+++ event-streaming/src/networking/linux/tcpSocketMessenger.cpp
@@ -3,17 +3,39 @@
 #include <iostream>
 #include <stdexcept>
 #include <sys/socket.h>
+#include "../../application/utils.h"
 
 TcpSocketMessenger::TcpSocketMessenger(TcpConnectionPool& tcpConnectionPool)
 	: m_TcpConnectionPool(tcpConnectionPool)
 {
 }
 
+// Used for generating TCP message buffer. Returns a pointer to allocated buffer
+void* FormTcpMessage(const std::string& message, uint32_t* bufferSize)
+{
+	LOG_TRACE("Entered FormTcpMessage");
+	uint32_t messageSize = message.length();
+	// 4 additional bytes are allocated for a uint32 to specify message size and establish
+	// TCP message boundaries
+	*bufferSize = 4 + messageSize;
+	void* buffer = malloc(*bufferSize);
+	uint32_t* sizePointer = reinterpret_cast<uint32_t*>(buffer);
+	*sizePointer = HostToBigEndian32(messageSize);
+	void* messagePointer = reinterpret_cast<void*>(sizePointer + 1);
+	memcpy(messagePointer, message.c_str(), messageSize);
+	LOG_DEBUG("TCP message size: {}", *bufferSize);
+	LOG_DEBUG("User message size: {}", messageSize);
+	return buffer;
+}
+
 bool TcpSocketMessenger::SendRequest(const std::vector<unsigned int>& targetSockets, std::string message) const
 {
 	LOG_TRACE("Entered TcpSocketMessenger::SendRequest");
 	LOG_DEBUG("Sending message '{}' to {} sockets", message, targetSockets.size());
-	
+
+	uint32_t bufferSize;
+	void* messageBuffer = FormTcpMessage(message, &bufferSize);
+
 	for (auto socket : targetSockets)
 	{
 		if (!m_TcpConnectionPool.HasClientSocket(socket))
@@ -22,12 +44,13 @@ bool TcpSocketMessenger::SendRequest(const std::vector<unsigned int>& targetSock
 			return false;
 		}
 
-		const char* messageBuffer = message.c_str();
-		if (send(socket, messageBuffer, message.length(), 0) == -1)
+		if (send(socket, messageBuffer, bufferSize, 0) == -1)
 		{
 			LOG_ERROR("Send request failed: '{}'", std::strerror(errno));
 			return false;
 		}
 	}
+
+	delete(messageBuffer);
 	return true;
 }
