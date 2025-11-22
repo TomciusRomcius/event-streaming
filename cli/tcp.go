b8aa79d578f42ebb9a409fb308e5660d12b9201f
tomas <tomas.ew88@gmail.com>
2025-11-22 16:22:27 +0200

    SendMessageBytes implementation

--- cli/tcp.go
+++ cli/tcp.go
@@ -29,3 +29,13 @@ func (cm *TcpService) SendMessage(message string) {
 	copy(socketBuffer[4:], messageBuf)
 	cm.connection.Write(socketBuffer)
 }
+
+func (cm *TcpService) SendMessageBytes(message []byte) {
+	var bufferSize = len(message)
+	var socketBuffer []byte = make([]byte, bufferSize+4)
+	var sizeBuffer []byte = make([]byte, 4)
+	binary.BigEndian.PutUint32(sizeBuffer, uint32(bufferSize))
+	copy(socketBuffer[:4], sizeBuffer)
+	copy(socketBuffer[4:], message)
+	cm.connection.Write(socketBuffer)
+}
