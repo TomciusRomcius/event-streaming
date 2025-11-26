20444d7c01e77393c49c88302aafde492f95cf8e
tomas <tomas.ew88@gmail.com>
2025-11-26 19:20:47 +0200

    Remove std cout in man func

--- event-streaming/src/main.cpp
+++ event-streaming/src/main.cpp
@@ -1,10 +1,7 @@
-﻿#include <iostream>
-#include "application/application.h"
+﻿#include "application/application.h"
 
 int main()
 {
-    std::cout << "Event streaming application started." << std::endl;
-
     Application app;
     app.Start();
     return 0;
